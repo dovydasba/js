21b38be007480ef7913acf9b97c1642c1a792aaa
GediminasGricius <g.gricius@ituostas.lt>
2024-03-18 10:56:06 +0200

    Kintamieji

---
+++ js1/daugyba.js
@@ -0,0 +1,6 @@
+let x=Number(  process.argv[2] );
+let y=Number( process.argv[3] );
+
+console.log(x*y);
+
+
---
+++ js1/sudetis.js
@@ -0,0 +1,7 @@
+let x=Number(  process.argv[2] );
+let y=Number( process.argv[3] );
+
+console.log(x+y);
+
+
+
